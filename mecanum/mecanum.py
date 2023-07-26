85cf6d62724dcdbe786699dafdfc15db1c585db4
kientr2002 <kien.tranvictory@hcmut.edu.vn>
2023-07-26 17:42:08 +0700

    add draft of go left and go straight

--- mecanum/mecanum.py
+++ mecanum/mecanum.py
@@ -11,19 +11,63 @@ from mecanum import *
 driver = DCMotor(machine.SoftI2C(scl=machine.Pin(22), sda=machine.Pin(21), freq=100000))
 
 # Mô tả hàm này...
-def r_E1_BA_BD_tr_C3_A1i():
-  global _C4_90_E1_BA_BFm_line__C4_91en_5_v_E1_BA_A1ch, S_E1_BA_B5n_s_C3_A0ng_, _C4_90_E1_BA_BFm_line__C4_91en_2_v_E1_BA_A1ch
-  display.show(Image("00000:00000:11100:00100:00100"))
+def R_E1_BA_BD_tr_C3_A1i():
+  global Function, Ready, LineRotate, DemLine2
+  if Ready == 0:
+    display.show(Image("00000:01000:11111:01000:00000"))
+    time.sleep_ms(1000)
+    Ready = 1
+  else:
+    display.scroll(LineRotate)
+    if mecanum.read_line_sensors() == (0, 1, 1, 0):
+      LineRotate = (LineRotate if isinstance(LineRotate, (int, float)) else 0) + 1
+      if LineRotate == 2:
+        display.set_all('#ff0000')
+        driver.setSpeed(0,0)
+        driver.setSpeed(1,0)
+        driver.setSpeed(2,0)
+        driver.setSpeed(3,0)
+        wait_for(lambda: (button_a.is_pressed()))
+        LineRotate = 0
+        Ready = 0
+        Function = 2
+      else:
+        driver.setSpeed(0,(-30))
+        driver.setSpeed(1,(-30))
+        driver.setSpeed(2,30)
+        driver.setSpeed(3,30)
+        time.sleep_ms(200)
+    else:
+      driver.setSpeed(0,(-30))
+      driver.setSpeed(1,(-30))
+      driver.setSpeed(2,30)
+      driver.setSpeed(3,30)
 
 # Mô tả hàm này...
-def _C4_90i_th_E1_BA_B3ng_2_v_E1_BA_A1ch__C4_91en():
-  global _C4_90_E1_BA_BFm_line__C4_91en_5_v_E1_BA_A1ch, S_E1_BA_B5n_s_C3_A0ng_, _C4_90_E1_BA_BFm_line__C4_91en_2_v_E1_BA_A1ch
-  if S_E1_BA_B5n_s_C3_A0ng_ == 0:
+def C_C4_83n_ch_E1_BB_89nh_r_E1_BA_BD_tr_C3_A1i():
+  global Function, Ready, LineRotate, DemLine2
+  if mecanum.read_line_sensors() == (1, 1, 1, 1):
+    driver.setSpeed(0,0)
+    driver.setSpeed(1,0)
+    driver.setSpeed(2,0)
+    driver.setSpeed(3,0)
+    wait_for(lambda: (button_a.is_pressed()))
+    Function = 0
+  else:
+    driver.setSpeed(0,25)
+    driver.setSpeed(1,25)
+    driver.setSpeed(2,35)
+    driver.setSpeed(3,35)
+
+# Mô tả hàm này...
+def _C4_90i_th_E1_BA_B3ng():
+  global Function, Ready, LineRotate, DemLine2
+  if Ready == 0:
     display.show(Image("00100:01110:00100:00100:00100"))
     time.sleep_ms(1000)
-    S_E1_BA_B5n_s_C3_A0ng_ = 1
+    Ready = 1
   else:
-    display.scroll(_C4_90_E1_BA_BFm_line__C4_91en_2_v_E1_BA_A1ch)
+    display.scroll(DemLine2)
     if (mecanum.read_line_sensors() == (0, 0, 1, 0)) or (mecanum.read_line_sensors() == (0, 1, 1, 0)):
       driver.setSpeed(0,30)
       driver.setSpeed(1,30)
@@ -55,16 +99,17 @@ def _C4_90i_th_E1_BA_B3ng_2_v_E1_BA_A1ch__C4_91en():
       driver.setSpeed(2,20)
       driver.setSpeed(3,20)
     elif (mecanum.read_line_sensors() == (1, 1, 1, 1)) or (mecanum.read_line_sensors() == (1, 0, 1, 1)) or (mecanum.read_line_sensors() == (1, 1, 0, 1)) or (mecanum.read_line_sensors() == (1, 1, 1, 0)) or (mecanum.read_line_sensors() == (0, 1, 1, 1)):
-      _C4_90_E1_BA_BFm_line__C4_91en_2_v_E1_BA_A1ch = (_C4_90_E1_BA_BFm_line__C4_91en_2_v_E1_BA_A1ch if isinstance(_C4_90_E1_BA_BFm_line__C4_91en_2_v_E1_BA_A1ch, (int, float)) else 0) + 1
-      if _C4_90_E1_BA_BFm_line__C4_91en_2_v_E1_BA_A1ch == 2:
+      DemLine2 = (DemLine2 if isinstance(DemLine2, (int, float)) else 0) + 1
+      if DemLine2 == 2:
         display.set_all('#ff0000')
         driver.setSpeed(0,0)
         driver.setSpeed(1,0)
         driver.setSpeed(2,0)
         driver.setSpeed(3,0)
         wait_for(lambda: (button_a.is_pressed()))
-        _C4_90_E1_BA_BFm_line__C4_91en_2_v_E1_BA_A1ch = 0
-        S_E1_BA_B5n_s_C3_A0ng_ = 0
+        DemLine2 = 0
+        Ready = 0
+        Function = 1
       else:
         driver.setSpeed(0,30)
         driver.setSpeed(1,30)
@@ -72,16 +117,17 @@ def _C4_90i_th_E1_BA_B3ng_2_v_E1_BA_A1ch__C4_91en():
         driver.setSpeed(3,23)
         time.sleep_ms(200)
     elif (mecanum.read_line_sensors() == (0, 1, 0, 1)) or (mecanum.read_line_sensors() == (1, 0, 1, 0)):
-      _C4_90_E1_BA_BFm_line__C4_91en_2_v_E1_BA_A1ch = (_C4_90_E1_BA_BFm_line__C4_91en_2_v_E1_BA_A1ch if isinstance(_C4_90_E1_BA_BFm_line__C4_91en_2_v_E1_BA_A1ch, (int, float)) else 0) + 1
-      if _C4_90_E1_BA_BFm_line__C4_91en_2_v_E1_BA_A1ch == 5:
+      DemLine2 = (DemLine2 if isinstance(DemLine2, (int, float)) else 0) + 1
+      if DemLine2 == 2:
         display.set_all('#ff0000')
         driver.setSpeed(0,0)
         driver.setSpeed(1,0)
         driver.setSpeed(2,0)
         driver.setSpeed(3,0)
         wait_for(lambda: (button_a.is_pressed()))
-        _C4_90_E1_BA_BFm_line__C4_91en_2_v_E1_BA_A1ch = 0
-        S_E1_BA_B5n_s_C3_A0ng_ = 0
+        DemLine2 = 0
+        Ready = 0
+        Function = 1
       else:
         driver.setSpeed(0,30)
         driver.setSpeed(1,30)
@@ -100,38 +146,16 @@ if True:
   driver.setSpeed(1,0)
   driver.setSpeed(2,0)
   driver.setSpeed(3,0)
-  _C4_90_E1_BA_BFm_line__C4_91en_5_v_E1_BA_A1ch = 0
-  _C4_90_E1_BA_BFm_line__C4_91en_2_v_E1_BA_A1ch = 0
-  S_E1_BA_B5n_s_C3_A0ng_ = 0
   wait_for(lambda: (button_a.is_pressed()))
+  DemLine2 = 0
+  Ready = 0
+  LineRotate = 0
+  Function = 0
 
 while True:
-  _C4_90i_th_E1_BA_B3ng_2_v_E1_BA_A1ch__C4_91en()
-
-
-driver.setSpeed(0,30)
-driver.setSpeed(1,30)
-driver.setSpeed(2,23)
-driver.setSpeed(3,23)
-driver.setSpeed(0,30)
-driver.setSpeed(1,30)
-driver.setSpeed(2,25)
-driver.setSpeed(3,25)
-
-driver.setSpeed(0,30)
-driver.setSpeed(1,(-30))
-driver.setSpeed(2,(-30))
-driver.setSpeed(3,30)
-
-'Lệch phải'
-
-'Ngang phải'
-
-'Lệch trái'
-
-driver.setSpeed(0,30)
-driver.setSpeed(1,30)
-driver.setSpeed(2,30)
-driver.setSpeed(3,30)
-
-display.show(Image("01000:11111:01001:00001:00001"))
+  if Function == 0:
+    _C4_90i_th_E1_BA_B3ng()
+  elif Function == 1:
+    R_E1_BA_BD_tr_C3_A1i()
+  else:
+    C_C4_83n_ch_E1_BB_89nh_r_E1_BA_BD_tr_C3_A1i()
