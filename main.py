971d424d9947af99c1f4bc968a97b0179c9ee8cb
kientr2002 <kien.tranvictory@hcmut.edu.vn>
2023-08-03 09:40:37 +0700

    convert to rover

--- main.py
+++ main.py
@@ -11,16 +11,18 @@ import time
 # CONFIG MQTT
 MQTT_SERVER = "mqtt.ohstem.vn"
 MQTT_PORT = 1883
-MQTT_USERNAME = "KhoiTruong9802"
+MQTT_USERNAME = "internK"
 MQTT_PASSWORD = "123456"
-MQTT_TOPIC_PUB = MQTT_USERNAME + "/feeds/V10"
-MQTT_TOPIC_SUB = MQTT_USERNAME + "/feeds/V11"
+MQTT_TOPIC_PUB = MQTT_USERNAME + "/feeds/V1"
+MQTT_TOPIC_PUB2 = MQTT_USERNAME + "/feeds/V2"
+MQTT_TOPIC_SUB = MQTT_USERNAME + "/feeds/V3"
 
 # CONFIG DEFAULT IMAGE WHEN THE CAMERA IS NOT OPEN
 DEFAULT_IMG = Image.open("assets/bo.bmp")
 
 CONFIDENCE_SCORE_CONFIRM = np.float64(80)
-TIMES_CONFIRM = 2
+TIMES_CONFIRM = 10
+AREA = 1
 
 root = tk.Tk()
 root.title("Robot app")
@@ -81,6 +83,8 @@ get_model = pathlib.Path("models")
 for item in get_model.iterdir():
     model_list.append(str(item)[7:])
 
+
+
 # Auto load model bienbao when program start
 def auto_load_model():
     global model, class_names, num_of_model_loaded
@@ -92,6 +96,7 @@ def auto_load_model():
     arr_model_name.append("bienbao")
     arr_class_names.append(class_names)
     num_of_model_loaded += 1
+
 
 # auto_load_model()
 
@@ -111,7 +116,7 @@ def my_load_model(model_choose):
             print("Load model " + model_name + " sucessful")
             message["text"] = "Load model " + model_name + " sucessful"
             return
-            
+
     # Keras path to load
     keras_path = "models/" + model_name + "/keras_Model.h5"
     # Class name path to load
@@ -137,14 +142,15 @@ def my_load_model(model_choose):
     print("Load model " + model_name + " sucessful")
     message["text"] = "Load model " + model_name + " sucessful"
 
+
 ###################################
 ### CAMERA AND PREDICT FUNCTION ###
 ###################################
 
 # Update frame and prediction
 def show_img():
-    global cam, ai_result, count_ai, count_ai_confirm
-    ret, frame = cam.read() # type: ignore
+    global cam, ai_result, count_ai, count_ai_confirm, AREA
+    ret, frame = cam.read()  # type: ignore
     cv2_img = DEFAULT_IMG
     if get_image_running:
         cv2_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
@@ -162,21 +168,27 @@ def show_img():
             image = (image / 127.5) - 1
 
             # Predicts the model
-            prediction = model.predict(image) # type: ignore
+            prediction = model.predict(image)  # type: ignore
             index = np.argmax(prediction)
-            class_name = class_names[index] # type: ignore
+            class_name = class_names[index]  # type: ignore
             confidence_score = prediction[0][index]
 
             # Print prediction and confidence score
             print("Class:", class_name[2:], end="")
             print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
-
+            message["text"] = "Class:" + class_name[2:] + " Value:" + str(np.round(confidence_score * 100))[:-2] + "%"
             if send_MQTT_running:
                 if confidence_score * 100 > CONFIDENCE_SCORE_CONFIRM and class_name[2:] == ai_result:
                     count_ai_confirm += 1
                     if count_ai_confirm >= TIMES_CONFIRM:
-                        mqttClient.publish(MQTT_TOPIC_PUB, ai_result, 0, True)
-                        count_ai_confirm = 0
+                        if AREA == 1:
+                            mqttClient.publish(MQTT_TOPIC_PUB, ai_result, 0, True)
+                            count_ai_confirm = 0
+                            AREA = 2
+                        elif AREA == 2:
+                            mqttClient.publish(MQTT_TOPIC_PUB2, ai_result, 0, True)
+                            count_ai_confirm = 0
+                            AREA = 3
                 else:
                     count_ai_confirm = 0
 
@@ -186,10 +198,11 @@ def show_img():
     cv2_img = cv2_img.resize((320, 240))
     cv2_img = ImageTk.PhotoImage(cv2_img)
     label_cv.config(image=cv2_img)
-    label_cv.image = cv2_img # type: ignore #VERY IMPORTANT
-    
+    label_cv.image = cv2_img  # type: ignore #VERY IMPORTANT
+
     if get_image_running:
         root.after(5, show_img)
+
 
 # Show camera
 def start_cam():
@@ -202,16 +215,18 @@ def start_cam():
     ##############
     ### CAM IP ###
     ##############
+    # cam = cv2.VideoCapture("http://192.168.1.56:81/stream")
     cam = cv2.VideoCapture(0)
-    # cam = cv2.VideoCapture(0)
     show_img()
+
 
 # Stop camera
 def stop_cam():
     global get_image_running, cam
     get_image_running = 0
-    cam.release() # type: ignore
+    cam.release()  # type: ignore
     cv2.destroyAllWindows()
+
 
 #####################
 ### MQTT FUNCTION ###
@@ -221,8 +236,10 @@ def mqtt_connected(client, userdata, flags, rc):
     print("Connected succesfully!!")
     client.subscribe(MQTT_TOPIC_SUB)
 
+
 def mqtt_subscribed(client, userdata, mid, granted_qos):
     print("Subscribed to Topic!!!")
+
 
 mqttClient = mqtt.Client()
 mqttClient.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
@@ -231,6 +248,7 @@ mqttClient.connect(MQTT_SERVER, int(MQTT_PORT), 60)
 # Register mqtt events
 mqttClient.on_connect = mqtt_connected
 mqttClient.on_subscribe = mqtt_subscribed
+
 
 # mqttClient.loop_start()
 
@@ -247,7 +265,10 @@ def send_to_MQTT():
     send_MQTT_running = 1
     print("START SENDING TO MQTT")
     message["text"] = "START SENDING TO MQTT"
+    global AREA
+    AREA = 1
     # MQTT_loop()
+
 
 # def MQTT_loop():
 #     global ai_result, send_MQTT_running
@@ -263,6 +284,7 @@ def close_send_MQTT():
     global send_MQTT_running
     send_MQTT_running = 0
 
+
 #################
 ### tkinter #####
 #################
@@ -271,7 +293,7 @@ def close_send_MQTT():
 main_screen = tk.Frame(container)
 main_screen.grid(row=0, column=0, padx=(0, 20))
 
-main_screen_label = ttk.Label(main_screen, text="MAIN SCREEN" , font=("Arial", 20))
+main_screen_label = ttk.Label(main_screen, text="MAIN SCREEN", font=("Arial", 20))
 main_screen_label.grid(row=0, column=0, pady=(0, 10))
 
 button_frame = tk.Frame(main_screen)
@@ -296,9 +318,9 @@ button_model_frame.grid(row=0, column=0, padx=(0, 10))
 
 model_choose = tk.StringVar()
 option_menu = ttk.OptionMenu(
-    button_model_frame, 
-    model_choose, 
-    "Not selected", 
+    button_model_frame,
+    model_choose,
+    "Not selected",
     *model_list,
     bootstyle="secondary"  # type: ignore
 )
@@ -314,16 +336,16 @@ button_cam_frame = ttk.Labelframe(button_frame, text="Camera", padding=5)
 button_cam_frame.grid(row=0, column=1, padx=(0, 10))
 
 button_start_cam = ttk.Button(
-    button_cam_frame, 
-    text="Start camera", 
+    button_cam_frame,
+    text="Start camera",
     bootstyle="success",  # type: ignore
     command=start_cam
 )
 button_start_cam.pack(side="top", fill="both", pady=(0, 10))
 
 button_stop_cam = ttk.Button(
-    button_cam_frame, 
-    text="Stop camera", 
+    button_cam_frame,
+    text="Stop camera",
     bootstyle="danger",  # type: ignore
     command=stop_cam
 )
